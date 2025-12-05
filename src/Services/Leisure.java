3ac2263496072bb4d396b0f55115d3df5eca9b4c
Alexandre da Silva Baptista <as.baptista@campus.fct.unl.pt>
2025-12-05 18:45:05 +0000

    Delete src directory

--- src/Services/Leisure.java
+++
@@ -1,9 +0,0 @@
-package Services;
-
-/**
- * Interface defining the specific contract for Leisure services.
- * It extends the base {@link Service} interface, adding functionalities
- * related to student discounts on ticket prices.
- */
-public interface Leisure extends Service {
-}
